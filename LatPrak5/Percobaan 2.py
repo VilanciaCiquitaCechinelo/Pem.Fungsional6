73c590766df6b649c5d0171ca758a291afca00a4
Vilancia Ciquita <127310358+VilanciaCiquitaCechinelo@users.noreply.github.com>
2023-12-14 10:23:03 +0700

    Add files via upload

---
+++ LatPrak5/Percobaan 2.py
@@ -0,0 +1,32 @@
+# TODO 0: Import beberapa library lain yang dibutuhkan
+from PIL import Image
+
+# TODO 1: Buka gambar latar belakang (background) dan gambar yang ingin disisipkan (overlay)
+background_image = Image.open(
+    "C:\\Users\\Vilancia\\Pictures\\images.jpg"
+)
+overlay_image = Image.open(
+    "C:\\Users\\Vilancia\\Pictures\\jerapah.jpg"
+)
+
+# TODO 2: Konversi overlay image ke mode RGB (menghilangkan alpha channel)
+overlay_image = overlay_image.convert("RGBA")
+
+# TODO 3: (optional) Perkecil ukuran gambar overlay menggunakan method resize()
+max_size = (300,300) 
+overlay_image.thumbnail(max_size)
+# Jika ingin meresize, uncomment baris berikut
+# overlay_image = overlay_image.resize((new_width, new_height))
+
+# Tentukan/Hitung koordinat tengah untuk menempatkan overlay
+x_center = (background_image.width - overlay_image.width) // 2
+y_center = (background_image.height - overlay_image.height) // 2
+
+# TODO 4: Sisipkan gambar overlay ke dalam gambar background menggunakan method paste()
+background_image.paste(overlay_image, (x_center, y_center))
+
+# TODO 5: Simpan gambar hasil edit
+background_image.save("per2.jpg")
+
+# TODO 6: Tampilkan
+background_image.show()
